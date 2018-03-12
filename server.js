af07a9b45b322a0932e96930c22f4c4638f5c8f3
Ahmad <ali333291@gmail.com>
2018-03-12 08:41:39 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+// Configuration
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set('view engine', 'hbs');
+
+
+//-- middlewares
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next(); // unless you dont call next, app will be stuck
+});
+
+// -- Maintentance
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+// Helpers
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {pageTitle: 'About Page'});
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({errorMessage: 'Unable to handle request'});
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
